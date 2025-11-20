096d6c4344a6596a399b59aa04f5d9fc14788014
Mesbah <iamaminemesbah@gmail.com>
2025-11-20 20:40:05 +0100

    fixed protected pages check

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,34 +1,48 @@
-import { createServerClient } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
-    request,
-  })
+	let supabaseResponse = NextResponse.next({
+		request,
+	});
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll()
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({
-            request,
-          })
-          cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
-          )
-        },
-      },
-    }
-  )
+	const supabase = createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+		{
+			cookies: {
+				getAll() {
+					return request.cookies.getAll();
+				},
+				setAll(cookiesToSet) {
+					cookiesToSet.forEach(({ name, value, options }) =>
+						request.cookies.set(name, value),
+					);
+					supabaseResponse = NextResponse.next({
+						request,
+					});
+					cookiesToSet.forEach(({ name, value, options }) =>
+						supabaseResponse.cookies.set(name, value, options),
+					);
+				},
+			},
+		},
+	);
 
-  // refreshing the auth token
-  await supabase.auth.getUser()
+	// refreshing the auth token
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 
-  return supabaseResponse
+	// redirect to login if user is not logged in and not in a public page
+	if (
+		!user &&
+		!['/signin', '/api/webhook', '/auth'].some((path) =>
+			request.nextUrl.pathname.startsWith(path),
+		)
+	) {
+		return NextResponse.redirect(new URL('/signin', request.url));
+	}
+
+	return supabaseResponse;
 }
